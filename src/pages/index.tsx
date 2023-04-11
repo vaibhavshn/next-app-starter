35e5a7a90fd44a4e2ddab24b8c7953ee7193ed72
Vaibhav Shinde <vaibhavshn.in@gmail.com>
2023-04-11 11:39:52 +0530

    chore: useTabs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <div className="text-center py-12 text-3xl text-blue-800 font-bold">
-      hello world
-    </div>
-  );
+	return (
+		<div className="text-center py-12 text-3xl text-blue-800 font-bold">
+			hello world
+		</div>
+	);
 }
